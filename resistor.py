e70fcd6a2622068774f328905ca6cfb7f97c9ca1
Xcine <unefb@student.kit.edu>
2017-06-29 17:06:17 +0200

    started editing the inverse problem

--- resistor.py
+++ resistor.py
@@ -1,4 +1,3 @@
-#from numpy import dot
 from pyb import I2C
 
 class Resistor(object):
@@ -10,15 +9,15 @@ class Resistor(object):
 		self.i2c = I2C(2, I2C.MASTER)
 		self.i2c.init(I2C.MASTER, baudrate=20000)
 		info = self.i2c.scan()
-		print("scan ",info)
+		#print("scan ",info)
 
 	def FindBinaryResistor(self, input = 1000.0):
 		input -= 1000.0
-		resistorBin = [0,0,0,0,0,0,0,0,0,0]
+		resistorBin = [1,1,1,1,1,1,1,1,1,1]
 
 		for x in range(10):
 			if input >= self.allResistors[x]:
-				resistorBin[x] = 1
+				resistorBin[x] = 0
 				input -= self.allResistors[x]
 			else:
 				pass
@@ -26,23 +25,31 @@ class Resistor(object):
 		return resistorBin
 
 	def SetResistor(self, input = 1000.0):
-		resistorBin = self.FindBinaryResistor(input)
-		self.currenResistorBinary = resistorBin
-		Bin1 = resistorBin[:2]
-		Bin2 = resistorBin[2:]
+
+		if (type(input) == int) or (type(input) == float): 
+			resistorBin = self.FindBinaryResistor(input)
+			self.currenResistorBinary = resistorBin
+			bin1 = resistorBin[:2]
+			bin2 = resistorBin[2:]
+		elif (type(input) == list) and len(input) == 10:
+			self.currenResistorBinary = input
+			bin1 = input[:2]
+			bin2 = input[2:]
+		else:
+			print("ERROR: wrong type oder length of array")
 
 		data = bytearray([0x02,0x46,0xFF,0x03])
-		data[2] = self.GetBinaryOfList(Bin2)
-		data[3] = self.GetBinaryOfList(Bin1)
+		data[2] = self.GetBinaryOfList(bin2)
+		data[3] = self.GetBinaryOfList(bin1)
 
 		print("setting resistor to", self.GetResistor(), "Ohm")
 		self.i2c.send(data,96)
 
 	def GetResistor(self):
-		return (self.dotProd(self.currenResistorBinary, self.allResistors) + 1000.0)
+		return (self.dotProd(self.negBinaryList(self.currenResistorBinary), self.allResistors) + 1000.0)
 
 	def FindResistor(self, input):
-		return (self.dotProd(self.FindBinaryResistor(input), self.allResistors) + 1000.0)
+		return (self.dotProd(self.negBinaryList(self.FindBinaryResistor(input)), self.allResistors) + 1000.0)
 
 	def GetBinaryOfList(self, input):
 		out = 0
@@ -58,6 +65,14 @@ class Resistor(object):
 
 		return sum
 
+	def negBinaryList(self, list):
+		for x in list:
+			if x == 0:
+				x = 1
+			else:
+				x = 0
+
+		return list
 
 
 
