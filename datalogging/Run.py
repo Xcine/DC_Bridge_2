07aaf2d3612e707e9f4faee6e50f3655d8d72994
Xcine <unefb@student.kit.edu>
2017-07-02 16:20:32 +0200

    test2

--- datalogging/Run.py
+++ datalogging/Run.py
@@ -5,27 +5,37 @@ import pyboard
 import serial
 
 def main():
+	#pyb1 = pyboard.Pyboard('/dev/tty.usbmodem1412')
+	#pyb1.enter_raw_repl()
+	#pyb1.exec('print("Hallo")')
+	#pyb1.follow()
+	#pyb1.exec('printsome()')
+	#pyb1.exec("import pyb")
+	#pyb1.exec("pyb.LED(3).toggle()")
+	#pyb1.exec("pyb.delay(1000)")
+	#pyb1.exec("pyb.LED(3).toggle()")
+	#pyb1.exit_raw_repl()
+	#pyb1.close()
+
 	#pyboard.execfile("test.py", device = "/dev/tty.usbmodem1412")
-	port = '/dev/tty.usbmodem1412'
 
-	print('Opening serial port {}'.format(port))
-	serial_port = serial.Serial(port=port)
+	#port = '/dev/tty.usbmodem1412'
+	#print('Opening serial port {}'.format(port))
+	#serial_port = serial.Serial(port=port)
+	#line = pyb1.serial.readline().strip()
+	#print(line)
+	#print(int(line)+1)
+	#pyb1.close()
 
-	line = serial_port.readline().strip()
-	print(line)
-	print(int(line)+1)
+	pyb1 = pyboard.Pyboard(device = "/dev/tty.usbmodem1412")
+	pyb1.enter_raw_repl()
+	output = pyb1.execfile("test.py")
+	pyboard.stdout_write_bytes(output)
+	pyb1.exit_raw_repl()
+	pyb1.close()
 
-	"""
-    pyb = pyboard.Pyboard('/dev/tty.usbmodem1412')
-    pyb.enter_raw_repl()
-    pyb.exec('import pyb')
-    pyb.exec('sw = pyb.Switch()')
-    pyb.exec('sw.callback(lambda:print('press!'))')
-    pyb.exec("sw()")
-    pyb.exec('pyb.LED(1).on()')
-    pyb.exec('print("Hallo")')
-    pyb.exit_raw_repl()
-	"""
+	print("_______________")
+	sys.stdout.buffer.write(output)
 
 if __name__ == '__main__':
     main()
--- datalogging/test.py
+++ datalogging/test.py
@@ -1,11 +1,1 @@
-import pyb
-import time
-
-sw = pyb.Switch()
-i = 0
-
-while not sw():
-	print(i)
-	time.sleep(1)
-	i += 1
-
+printsome()
